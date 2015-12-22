e3c5ddc814a141ca0d815078c998dfe63e00fefe
Willem <tulipboy@gmail.com>
2015-12-22 21:00:20 +0100

    De eerste opdracht

---
+++ ToegestaneVariabeleNamen/OefeningVariabeleNamen.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package OpdrachtenWeek2.ToegestaneVariabeleNamen;
+
+/**
+ *
+ * @author Admin
+ */
+public class OefeningVariabeleNamen {
+    
+    public static void main(String[] args) {
+        // hier worden enkele variabele aangemaakt om te kijken of de namen worden geaccepteerd:
+        String isChanged;
+        String $name;
+        String €name;
+        String Name;
+        String string;
+        String For;
+        String While;
+        String _address;
+        String __4444;
+        
+        // en nu volgen enkele namen die niet behoren te werken:
+        String 5name;
+        String #name;
+        String for;
+        String while;
+        String switch;
+        // en zoals verwacht krijg ik meteen allemaal errors als ik het alleen al aan het typen ben.
+        
+    }
+    
+}
